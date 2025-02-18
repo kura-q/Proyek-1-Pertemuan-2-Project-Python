caf03211fa7f12acf7586274612437cbd2d3095f
isanAje <153609998+SanzAje@users.noreply.github.com>
2025-02-18 13:39:07 +0700

    konvertCm.py

--- konvertCm.py
+++ konvertCm.py
@@ -2,15 +2,17 @@ def konvertCm(angka) :
     pilihan = input("Konvert ke (KM, DAM, M, DM, CM, MM): ").upper()
 
     if pilihan == "KM" :
-        angka = angka // 10
+        angka = angka // 100000
+    elif pilihan == "HM"
+        angka == angka // 10000
     elif pilihan == "DAM" :
-        angka = angka * 10
+        angka = angka // 1000
     elif pilihan == "M" :
-        angka = angka * 100
+        angka = angka // 100
     elif pilihan == "DM" :
-        angka = angka * 1000
+        angka = angka // 10
     elif pilihan == "MM" :
-        angka = angka * 100000
+        angka = angka * 10
     else :
         print("Pilihan tidak valid")
         return None
